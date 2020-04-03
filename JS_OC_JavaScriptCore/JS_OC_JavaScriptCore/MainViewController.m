8971b10fec834258cfdb7ee1ab6a35395f2bfa0b
chenfei <chenfei@bilibili.com>
2020-04-03 18:59:42 +0800

    555

--- JS_OC_JavaScriptCore/JS_OC_JavaScriptCore/MainViewController.m
+++ JS_OC_JavaScriptCore/JS_OC_JavaScriptCore/MainViewController.m
@@ -19,7 +19,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
     self.title = @"主页";
-    NSLog(@"222");
+    NSLog(@"2223");
 }
 
 - (IBAction)btnClick1:(id)sender {
